38eeeefa1c228294ef749feaef6e9f5baf1a0a69
ralferlebach <ralf.erlebach@gmx.de>
2026-03-30 08:54:56 +0200

    fix unit test bug

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_stackmathgame';
-$plugin->version   = 2026032612;
+$plugin->version   = 2026032701;
 $plugin->requires  = 2024100700; // Moodle 4.5.
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.5.0';
